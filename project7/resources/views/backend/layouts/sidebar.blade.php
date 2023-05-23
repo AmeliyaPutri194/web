6031a5816c0f103886cb88abbcfb59bd280d0de0
ameliyaPutri <amlyputrii@gmail.com>
2023-05-23 15:03:39 +0700

    project

---
+++ project7/resources/views/backend/layouts/sidebar.blade.php
@@ -0,0 +1,41 @@
+<!-- ======= Sidebar ======= -->
+<aside id="sidebar" class="sidebar">
+
+<ul class="sidebar-nav" id="sidebar-nav">
+
+  <li class="nav-item">
+    <a class="nav-link " href="index.html">
+      <i class="bi bi-grid"></i>
+      <span>Dashboard</span>
+    </a>
+  </li><!-- End Dashboard Nav -->
+  
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="users-profile.html">
+      <i class="bi bi-person"></i>
+      <span>Profile</span>
+    </a>
+  </li><!-- End Profile Page Nav -->
+
+<li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-layout-text-window-reverse"></i><span>Riwayat Hidup</span><i
+                    class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="tables-general.html">
+                        <i class="bi bi-circle"></i><span>Pendidikan</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ url('pengalaman_kerja') }}">
+                        <i class="bi bi-circle"></i><span>Pengalaman Kerja</span>
+                    </a>
+                </li>
+            </ul>
+        </li><!-- End Tables Nav -->
+
+</ul>
+
+</aside><!-- End Sidebar-->
